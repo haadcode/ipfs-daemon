ccd3f59c30c1460baa99384318e8cb893b21564e
haad <haad@headbanggames.com>
2016-11-17 10:42:46 +0100

    Update examples

--- examples/native.js
+++ examples/native.js
@@ -8,6 +8,7 @@ const ipfs = new IPFS()
 ipfs.on('error', (e) => console.error(e))
 ipfs.on('ready', () => {
     console.log("Hello, Interplanetary Friend")
+    console.log(ipfs.PeerId)
     console.log(ipfs.GatewayAddress)
     console.log(ipfs.APIAddress)
     ipfs.stop()
